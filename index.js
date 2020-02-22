a1e66ef24d001eb62b4b87befaf49b05d619c3a1
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-22 14:27:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
- return "console.log( '"Happy holidays, ${name}!"";
+ return "Happy holidays, ${name}!";
 }
 
 function happyCustoHolidayTo(holiday, name){
